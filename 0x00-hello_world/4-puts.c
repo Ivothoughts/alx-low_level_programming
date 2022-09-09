de55abb758ddbd0dc7ffc18b9475ed2cc472544b
Ivothoughts <omoleifeoluwanimi@gmail.com>
2022-09-08 18:48:24 -0700

    phew

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,7 +6,7 @@
 int	main(void)
 {
 	/*using	the	puts	function*/
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 
 	return (0);
 }
