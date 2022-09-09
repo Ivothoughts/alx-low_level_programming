c4010676c63f4d0457f57c425617b87552985088
Ivothoughts <omoleifeoluwanimi@gmail.com>
2022-09-08 19:54:33 -0700

    do hard things

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+* main - main block
+* Return: 0
+*/
+int main(void)
+{
+	char c;
+	int i;
+	long li;
+	long long lli;
+	float f;
+
+	printf("size of a char: %ld byte(s)\n", sizeof(c));
+	printf("size of an int: %ld byte(s)\n", sizeof(i));
+	printf("size of a long int: %ld byte(s)\n", sizeof(li));
+	printf("size of a long long int: %ld byte(s)\n", sizeof(lli));
+	printf("size of a floaty: %ld byte(s)\n", sizeof(f));
+	return (0);
+}
