6457c487273416fa1c56d32378449b722d51ba22
Ivothoughts <omoleifeoluwanimi@gmail.com>
2022-09-08 17:59:46 -0700

    task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int puts(const char *str);
+{
+	/*using the puts function*/
+	char Mystr[] = "Programming is like building a multilingual puzzle,\n";
+
+	puts(Mystr);
+
+	return (0);
+}
