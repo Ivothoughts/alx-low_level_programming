ac027eb099dc18f083b782b5f872d8d50c0c5159
Ivothoughts <omoleifeoluwanimi@gmail.com>
2022-09-09 19:13:27 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ * main - prints a hexadecimal string
+ *
+ * Description: using the main function
+ * this program prints a hexadecimal string
+ * Return: 0
+ */
+int main(void)
+{
+	char c;
+
+	for (c = '0'; c <= '9'; c++)
+	{
+		putchar(c);
+	}
+
+	for (c = 'a'; c <= 'f'; c++)
+	{
+		putchar(c);
+	}
+
+	putchar('\n');
+	return (0);
+}
