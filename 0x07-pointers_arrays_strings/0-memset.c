a9e1beb4883e51a169785bdbe7a9cf8b2d4b1c01
Ivothoughts <omoleifeoluwanimi@gmail.com>
2022-09-27 21:14:02 -0700

    now right

--- 0x07-pointers_arrays_strings/0-memset.c
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -13,15 +13,21 @@
 
 
 char *_memset(char *s, char b, unsigned int n)
+
 {
 
-		unsigned int i;
-			for (i = 0; n > 0; i++, n--)
-					{
+	unsigned int i;
 
-								s[i] = b;
-									}
 
-				return (s);
 
+	for (i = 0; n > 0; i++, n--)
+
+	{
+
+		s[i] = b;
+
+	}
+
+
+	return (s);
 }
