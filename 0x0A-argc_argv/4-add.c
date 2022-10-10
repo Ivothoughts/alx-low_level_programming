0c4de2c3ec14388123682d00e0216c2dcf09d34e
Ivothoughts <omoleifeoluwanimi@gmail.com>
2022-10-09 17:20:01 -0700

    adding

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -22,12 +22,8 @@
 int main(int argc, char *argv[])
 
 {
-
 	int i, j, length, sum;
-
 	char *ptr;
-
-
 
 	if (argc < 2)
 
@@ -36,47 +32,29 @@ int main(int argc, char *argv[])
 	else
 
 	{
-
 		sum = 0;
 
 		for (i = 1; i < argc; i++)
 
 		{
-
 			ptr = argv[i];
-
 			length = strlen(ptr);
 
 
-
 			for (j = 0; j < length; j++)
-
 			{
-
 				if (isdigit(*(ptr + j)) == 0)
 
 				{
-
 					printf("Error\n");
 
 					return (1);
-
 				}
-
 			}
 
-
-
 			sum += atoi(argv[i]);
-
 		}
-
-
-
 		printf("%d\n", sum);
-
 	}
-
 	return (0);
-
 }
