b9f07e3cf266216ed47d024c724e3b4dd9c9e8ba
Ivothoughts <omoleifeoluwanimi@gmail.com>
2022-10-12 01:28:30 -0700

    sum

---
+++ 0x0D-preprocessor/4-sum.h
@@ -0,0 +1,13 @@
+#ifndef SUM_H
+
+#define SUM_H
+
+/*
+* file: 4-sum.h
+* Description: Header file defines a macro named SUM(x, y)
+*  that computes the sum of the numbers x and y
+*/
+
+#define SUM(x, y)  ((x) + (y))
+
+#endif
