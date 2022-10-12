be963de908d9a34583ca9e2e62b6408bf2c6ca20
Ivothoughts <omoleifeoluwanimi@gmail.com>
2022-10-12 00:04:04 -0700

    new

--- 0x0D-preprocessor/0-object_like_macro.h
+++ 0x0D-preprocessor/0-object_like_macro.h
@@ -5,14 +5,10 @@
 
 
 /*
- *
- *  * File: 0-object_like_macro.h
- *
- *   * Desc: Header file that defines a macro named
- *
- *    *       SIZE as an abbreviation for the token 1024.
- *
- *     */
+ * File: 0-object_like_macro.h
+ * Desc: Header file that defines a macro named
+ *       SIZE as an abbreviation for the token 1024.
+ */
 
 
 
